6e2876640ff508a1e8d99f38767c0baaa161dfc2
Joathã Andrade <joathaandrade2@gmail.com>
2021-06-14 10:22:09 -0300

    Atualizando

--- App.js
+++ App.js
@@ -5,10 +5,26 @@ const bodyParser = require("body-parser")
 const app = express()
 const admin = require("./routes/admin")
 const parth = require("path")
-//const mongoose = require("mongoose")
+const mongoose = require("mongoose")
+const session = require("express-session")
+const flash = require("connect-flash")
 
 //Configurações
+    //Sessao
+    app.use(session({
+        secret: "joathadesenvolvedor",
+        resave: true,
+        saveUninitialized:true
+    }))
+    app.use(flash())
 
+    //Middleware
+    app.use((req, res, next)=>{
+        res.locals.success_msg = req.flash("success_msg")
+        res.locals.error_msg = req.flash("error_msg")
+        next()
+    })
+    
    //Handlebars
     app.engine('handlebars', handlebars({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
@@ -17,8 +33,20 @@ const parth = require("path")
     app.use(express.json())
 
     //Mongosse
+    mongoose.connect("mongodb://localhost/blogapp", {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    }).then(()=>{
+        console.log("Conectado ao mongo")
+    }).catch((err)=>{
+        console.log("Erro ao se conectar " +err)
+    })
 //Public
     app.use(express.static(parth.join(__dirname, "public")))
+
+    
 
 
 //Rotas
